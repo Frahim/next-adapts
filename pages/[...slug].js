9117cc22d719036ded679102d0af1fa3ff89d9e8
Md Yeasir Arafat <frahim5@gmail.com>
2022-11-02 20:54:10 +0600

    up

---
+++ pages/[...slug].js
@@ -0,0 +1,36 @@
+
+import DOMPurify from 'isomorphic-dompurify';
+
+
+function Slug({ pageitem }) {
+
+   
+        return (
+            <>
+             <div className="product-des" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(pageitem[0].content.rendered) }}></div> 
+            {console.log(pageitem[0].slug)} 
+            </>
+        );
+     
+    
+}
+
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch('https://www.adaptstoday.co.uk/wp-json/wp/v2/pages?slug='+ context.params.slug);
+    const pageitem = await res.json();
+   // const match = pageitem[0].slug
+   
+        return {
+            props: {
+                pageitem,
+                fallback: false,  
+            }
+         
+        }
+
+    
+
+}
+
+export default Slug;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import HeaderComponent from '../component/comon/header'
+import Footer from '../component/comon/footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <HeaderComponent/>
+    <Component {...pageProps}/>
+    <Footer/>
+     </>
+  )  
+  
 }
 
 export default MyApp
